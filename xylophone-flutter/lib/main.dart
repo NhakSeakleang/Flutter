22ce670996e60626279fdb15e9e2fa0a119b808d
NhakSeakleang <nhakseakleang@gmail.com>
2019-10-29 22:50:37 +0700

    finished play sound

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -4,20 +4,40 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  final AudioCache player = AudioCache();
+
+  void playSound(int number) {
+    player.play('note$number.wav');
+  }
+
+  List<Widget> setUpButton() {
+    var listButton = <Widget>[];
+    var colorsButton = [Colors.red, Colors.orange, Colors.yellow, Colors.green, Colors.teal, Colors.blue, Colors.purple];
+    for (int i = 1; i<=7; i++) {
+      listButton.add(Expanded(
+        child: FlatButton(
+          color: colorsButton[i - 1],
+          onPressed: () {
+            playSound(i);
+          },
+          child: Text('Note $i'),
+        ),
+      ),);
+    }
+    return listButton;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final AudioCache player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: setUpButton(),
           ),
         ),
       ),
